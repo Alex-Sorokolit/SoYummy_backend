3e8278eaff5e6684c16d9819510125c90aacc7f3
Alex-Sorokolit <sorokolital@gmail.com>
2023-03-30 11:50:12 +0300

    create server create db and connect

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require("colors");
+
+const connectDb = async () => {
+  try {
+    const db = await mongoose.connect(process.env.DB_URI);
+    console.log(
+      `mongodb is connected, dbName: ${db.connection.name}, onPort: ${db.connection.port}, onHost: ${db.connection.host}`
+        .white.bold.bgGreen
+    );
+  } catch (error) {
+    console.log(error.message.white.bgRed.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
+
+// const Cat = mongoose.model("Cat", { name: String });
+
+// const kitty = new Cat({ name: "Zildjian" });
+// kitty.save().then(() => console.log("meow"));
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const connectDb = require("./config/db");
+require("colors");
+
+// отримуємо шлях до файлу .env
+const configPath = path.join(__dirname, "config", ".env");
+dotenv.config({ path: configPath });
+
+// створення сервера
+const app = express();
+
+connectDb();
+
+// отримуємо порт і запускаємо сервер
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(
+    `server is running on port: , ${process.env.PORT}`.white.bgCyan.bold
+  );
+});
