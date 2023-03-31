f61cf0c6d60a53b44350468d2891235d1c027078
Alex-Sorokolit <sorokolital@gmail.com>
2023-03-31 12:03:52 +0300

    cors enable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 const connectDb = require("./config/db");
+const cors = require("cors");
 require("colors");
 
 // отримуємо шлях до файлу .env
@@ -11,12 +12,21 @@ dotenv.config({ path: configPath });
 // створення сервера
 const app = express();
 
+// Дозволяємо крос баузерні запити
+app.use(cors());
+
 // set routes
 app.use("/api/v1", require("./routes/recipesRoutes"));
 
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Not found",
+  });
+});
+
 connectDb();
 // отримуємо порт і запускаємо сервер
-const { PORT } = process.env;
+const { PORT = 5000 } = process.env;
 app.listen(PORT, () => {
   console.log(
     `server is running on port: , ${process.env.PORT}`.white.bgCyan.bold
