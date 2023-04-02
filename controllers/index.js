01bb57ada62d267c032f188d523ee7dc24a19e9c
ZEugenii <zinich911@gmail.com>
2023-04-02 09:06:59 +0300

    create logout route for user

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const controllers = require("./AuthController");
 module.exports = {
   register: controllers.register,
   login: controllers.login,
+  getCurrent: controllers.getCurrent,
+  logout: controllers.logout,
 };
 
 // const { recepiesController } = require("./RecipesController");
