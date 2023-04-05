af3aa109f967249040a0b2230028fd43c37835b2
ViacheslavMiakota <miakotaviacheslav1@gmail.com>
2023-04-05 10:35:32 +0300

    fix

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const recipesRouter = require("./routes/recipesRoutes");
 const ingredientsRouter = require("./routes/ingredientsRoutes");
 const searchRouter = require("./routes/searchRoutes");
 const popularRecipeRouter = require("./routes/popularRecipeRoutes");
-const ownRecipesRouter = require("./routes/ownRecipesRoutes");
-
+// const ownRecipesRouter = require("./routes/ownRecipesRoutes");
 const favoritesRouter = require("./routes/favoritesRoutes");
 
 require("colors");
@@ -33,10 +32,9 @@ app.use("/api/v1", recipesRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/ingredients", ingredientsRouter);
 app.use("/api/v1", searchRouter);
-app.use("/api/v1", favoritesRouter); popularRecipe
+app.use("/api/v1", favoritesRouter);
 app.use("/api/v1", popularRecipeRouter);
-app.use("./api/v1", ownRecipesRouter);
-
+// app.use("./api/v1", ownRecipesRouter);
 
 // Catch Errors ______________________________
 // обробка помилки 404
@@ -59,5 +57,5 @@ connectDb();
 // отримуємо порт і запускаємо сервер
 const { PORT = 5000 } = process.env;
 app.listen(PORT, () => {
-  console.log(`server is running on port: , ${process.env.PORT}`.white.bgCyan.bold)
+  console.log(`server is running on port: , ${process.env.PORT}`.white.bgCyan.bold);
 });
