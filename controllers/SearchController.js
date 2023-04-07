147c90db21eb35afe98b6473596721942433972b
Alex-Sorokolit <sorokolital@gmail.com>
2023-04-07 20:40:13 +0300

    search by fix

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -3,88 +3,26 @@ const Ingredients = require("../models/ingredientsModels");
 const asyncHandler = require("express-async-handler");
 
 class SearchController {
-  async find(req, res) {
-    // всі параметри пошуку
-    // console.log(req.query);
-
+  async findByTitle(req, res) {
     // дай мені page сторінку якщо на сторінці limit об'єктів
-    const { type = "Title", query = "", page = 1, limit = 20 } = req.query;
+    const { query = "", page = 1, limit = 20 } = req.query;
     const skip = (page - 1) * limit;
     const parsedLimit = parseInt(limit, 10);
-    if (type === "Ingredients") {
-      try {
-        const regex = new RegExp(query, "i");
-        const result = await Recipe.aggregate([
-          {
-            $lookup: {
-              from: "ingredients",
-              localField: "ingredients._id",
-              foreignField: "_id",
-              as: "ingredients",
-            },
-          },
-          {
-            $match: {
-              "ingredients.ttl": {
-                $regex: regex,
-              },
-            },
-          },
-        ])
-          .skip(skip)
-          .limit(parsedLimit);
 
-        const count = await Recipe.aggregate([
-          {
-            $lookup: {
-              from: "ingredients",
-              localField: "ingredients._id",
-              foreignField: "_id",
-              as: "ingredients",
-            },
-          },
-          {
-            $match: {
-              "ingredients.ttl": {
-                $regex: regex,
-              },
-            },
-          },
-          {
-            $count: "count",
-          },
-        ]);
-
-        const total = count.length ? count[0].count : 0;
-
-        res.status(200).json({
-          code: 200,
-          message: "success",
-          data: result,
-          quantity: result.length,
-          total: total,
-        });
-      } catch (error) {
-        res.status(404).json({
-          code: 404,
-          message: `${query} not found`,
-          error: error.message,
-        });
-      }
+    if (!query) {
+      res.status(400);
+      throw new Error("Controller: Please provide all required fields");
     }
 
     const result = await Recipe.find({
       title: { $regex: new RegExp(query, "i") },
     })
       .skip(skip)
-      .limit(limit);
+      .limit(parsedLimit);
 
-    if (!result && result.length === 0) {
+    if (!result) {
       res.status(404);
-      return res.json({
-        code: 404,
-        message: `${query} not found`,
-      });
+      throw new Error(`${query} not found`);
     }
 
     const totalCount = await Recipe.countDocuments({
@@ -99,10 +37,79 @@ class SearchController {
       total: totalCount,
     });
   }
+
+  async findByIngredient(req, res) {
+    // дай мені page сторінку якщо на сторінці limit об'єктів
+    const { query = "", page = 1, limit = 20 } = req.query;
+    const skip = (page - 1) * limit;
+    const parsedLimit = parseInt(limit, 10);
+
+    if (!query) {
+      res.status(400);
+      throw new Error("Controller: Please provide all required fields");
+    }
+    const regex = new RegExp(query, "i");
+    const result = await Recipe.aggregate([
+      {
+        $lookup: {
+          from: "ingredients",
+          localField: "ingredients._id",
+          foreignField: "_id",
+          as: "ingredients",
+        },
+      },
+      {
+        $match: {
+          "ingredients.ttl": {
+            $regex: regex,
+          },
+        },
+      },
+    ])
+      .skip(skip)
+      .limit(parsedLimit);
+
+    if (!result) {
+      res.status(404);
+      throw new Error(`${query} not found`);
+    }
+
+    const count = await Recipe.aggregate([
+      {
+        $lookup: {
+          from: "ingredients",
+          localField: "ingredients._id",
+          foreignField: "_id",
+          as: "ingredients",
+        },
+      },
+      {
+        $match: {
+          "ingredients.ttl": {
+            $regex: regex,
+          },
+        },
+      },
+      {
+        $count: "count",
+      },
+    ]);
+
+    const total = count.length ? count[0].count : 0;
+
+    res.status(200).json({
+      code: 200,
+      message: "success",
+      data: result,
+      quantity: result.length,
+      total: total,
+    });
+  }
 }
 
 const searchCtrl = new SearchController();
 
 module.exports = {
-  find: asyncHandler(searchCtrl.find),
+  findByTitle: asyncHandler(searchCtrl.findByTitle),
+  findByIngredient: asyncHandler(searchCtrl.findByIngredient),
 };
