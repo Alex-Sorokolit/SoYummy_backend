69d2501b70f2709c570011770a6b94735d7d9284
Alex-Sorokolit <sorokolital@gmail.com>
2023-04-12 12:54:04 +0300

    clean index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,9 @@
-// const controllers = require("./AuthController");
 const authCtrl = require("./AuthController");
 const recipeCtrl = require("./RecipesController");
 const ownRecipeCtrl = require("./OwnRecipesController");
 const shoppingCtrl = require("./ShoppingListController");
 
 module.exports = {
-  // register: controllers.register,
-  // login: controllers.login,
-  // getCurrent: controllers.getCurrent,
-  // logout: controllers.logout,
-  // getCurrentUser: controllers.getCurrentUser,
-  // updateUser: controllers.updateUser,
-  // updateAvatar: controllers.updateAvatar,
-  // subscription: controllers.subscription,
-  // googleAuth: controllers.googleAuth,
   authCtrl,
   recipeCtrl,
   ownRecipeCtrl,
